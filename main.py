0efe613b3502627c3b951f55689d20a66c5b4ed9
LucasSonego <sonego_lucas@outlook.com>
2018-12-08 19:10:28 -0200

    -criadas funçoes para teclas

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from teclas import *
+
+time.sleep(3)
+k_space(0)
+time.sleep(3)
+k_space(0.5)
+time.sleep(0)
+k_w(1)
+
+
+
---
+++ teclas.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pyautogui
+from pyautogui import *
+
+
+def k_space(delay):
+    pyautogui.keyDown('space')
+    time.sleep(delay)
+    pyautogui.keyUp('space')
+
+def k_w(delay):
+    pyautogui.keyDown('w')
+    time.sleep(delay)
+    pyautogui.keyUp('w')
+
+def k_a(delay):
+    pyautogui.keyDown('a')
+    time.sleep(delay)
+    pyautogui.keyUp('a')
+
+def k_s(delay):
+    pyautogui.keyDown('s')
+    time.sleep(delay)
+    pyautogui.keyUp('s')
+
+def k_d(delay):
+    pyautogui.keyDown('d')
+    time.sleep(delay)
+    pyautogui.keyUp('d')
+
